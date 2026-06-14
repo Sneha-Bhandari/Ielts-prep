c5b484c0be79df4a87aa23f7f51eaf4690050ad4
Aman Chhetri <kisanmahat2@gmail.com>
2026-06-14 11:44:46 +0545

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login'
-
+import  Course from './pages/(super_admin)/courses/course'
 function App() {
 
   return (
   <section className="hero">
+
+    <Routes>
+      <Route  path='/' element={<Login />} />
+      <Route path='/course' element={<Course />} />
+
+      
+
+    </Routes>
   <Login />
    
   </section>
